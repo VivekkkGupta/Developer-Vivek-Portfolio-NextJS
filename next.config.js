9874e8871330607cd03403cb82bb51153d796fec
Vivek <gupta1035@gmail.com>
2025-03-15 00:14:32 +0530

    some changes to deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: "export", // Required for static export
-  // Optional (ensures proper file generation)
+  output: "export", // Required for static export
+  images: { unoptimized: true }, // Fixes image issues in static export
+  trailingSlash: true, // Optional (ensures proper file generation)
 };
 
 module.exports = nextConfig;
